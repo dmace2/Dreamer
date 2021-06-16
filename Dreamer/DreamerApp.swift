805e50fa9cf696694490a9bd63b69915558f5846
Dylan Mace <dtmace2@gmail.com>
2021-06-16 11:43:32 -0400

    Initial Commit

---
+++ Dreamer/DreamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamerApp.swift
+//  Dreamer
+//
+//  Created by Dylan Mace on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct DreamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
